e07b35eae3c84777924e17cb900b9a2c105b297e
croethlisberger <mail@christoph-roethlisberger.ch>
2019-04-23 08:06:27 +0000

    23.04.2019 Initial commit

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("MetaflowImporter.MetaflowImporter.controller.MainView", {
+		onInit: function () {
+
+		}
+	});
+});
